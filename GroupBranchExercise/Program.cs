4ced2d0070c31aa70933f0c43be7c153d3811f82
Zachary Parr <zacharyparr@rocketmortgage.com>
2021-11-03 14:46:59 -0400

    first commit

---
+++ GroupBranchExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GroupBranchExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test project.");
+            Console.WriteLine("Or is it?");
+        }
+    }
+}
